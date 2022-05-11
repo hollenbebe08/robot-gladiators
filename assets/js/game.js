6bdb56dcb375105ee207145cf57f016b4f1752f8
Betsy <betsy.griffith12@gmail.com>
2022-05-11 19:03:07 -0400

    added for loop but the names are not yet showing up correctly

--- assets/js/game.js
+++ assets/js/game.js
@@ -3,15 +3,13 @@ var playerHealth= 100;
 var playerAttack= 10;
 var playerMoney= 10;
 
-//You can also log multiple values at once like this
-console.log(playerName, playerAttack, playerHealth);
 
-var enemyName= "Roborto";
+var enemyNames= ["Roborto", "Amy Android", "Robo Trumble"];
 var enemyHealth= 50;
 var enemyAttack= 12;
 
 //created fight function and this is an example of a function expression
-var fight=function() {
+var fight= function(enemyName) {
         //Alert players that they are starting the round
         window.alert("Welcome to Robot Gladiators!");
 
@@ -22,18 +20,18 @@ var fight=function() {
         if (promptFight === "fight" || promptFight === "FIGHT") {
             //remove enemy's health by subtracting the amount set in the playerAttack variable
             enemyHealth = enemyHealth - playerAttack;
-            console.log(playerName + " attacked " + enemyName + ". " + enemyName + " now has " + enemyHealth + " health remaining.");
+            console.log(playerName + " attacked " + enemyNames + ". " + enemyNames + " now has " + enemyHealth + " health remaining.");
 
         //check my enemy's health
         if (enemyHealth <= 0) {
-                window.alert(enemyName + " has died!");
+                window.alert(enemyNames + " has died!");
             } else {
-                window.alert(enemyName + " still has " + enemyHealth + " health left.");
+                window.alert(enemyNames + " still has " + enemyHealth + " health left.");
             }
 
         //remove player's health by subtracting the amount set in the enemyAttack variable
         playerHealth= playerHealth - enemyAttack;
-        console.log( enemyName + " attacked " + playerName + ". " + playerName + " now has " + playerHealth + " health remaining.");
+        console.log( enemyNames + " attacked " + playerName + ". " + playerName + " now has " + playerHealth + " health remaining.");
 
         //check player's health
         if(playerHealth <= 0) {
@@ -62,5 +60,21 @@ var fight=function() {
             }
     }
 
+    //for loop
+    for(var i=0; i < enemyNames.length; i++) {
+        console.log(enemyNames[i]);
+        console.log(i);
+        console.log(enemyNames[i] + " is at " + i + " index");
+    }
+
 //execute function
-fight();
+for(var i=0; i<enemyNames.length; i++) {
+    fight(enemyNames[i]);
+}
+//Game States
+
+// "WIN" - Player Robot has defeated all enemy-robots
+
+// *Fight all enemy-robots
+
+// "LOSE" - Player robot's health is zero or less
